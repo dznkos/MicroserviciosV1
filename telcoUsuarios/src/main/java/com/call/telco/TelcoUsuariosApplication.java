ea97f49e520a6a260c5dc61f825374da4e4c0631
Luis vcn <khanccer@gmail.com>
2023-01-31 23:30:06 -0500

    Micro v2

--- telcoUsuarios/src/main/java/com/call/telco/TelcoUsuariosApplication.java
+++ telcoUsuarios/src/main/java/com/call/telco/TelcoUsuariosApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan({"com.call.demo.model.entity"})
+@EntityScan({"com.call.telco.model.entity"})
 @SpringBootApplication
 public class TelcoUsuariosApplication {
 
